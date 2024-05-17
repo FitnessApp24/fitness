5c4a27033c8ca9a90041b75d63781dc314ba81f5
Sushanth <sp480s@att.com>
2024-05-17 12:57:16 +0530

    update env file

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAp3GFY2c52ALCL_UCyvRwmbYkVIQVlJtw",
-  authDomain: "fitness-59609.firebaseapp.com",
-  projectId: "fitness-59609",
-  storageBucket: "fitness-59609.appspot.com",
-  messagingSenderId: "1066707996271",
-  appId: "1:1066707996271:web:326fea1d3fc488287a079a",
-  measurementId: "G-42SMZ9DDZN"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MSG_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASURE_ID
 };
 
 // Initialize Firebase
