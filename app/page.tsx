c16676d1ee7f1a28b04314028be59588917a234a
FitnessApp24 <fitnessapp24@gmail.com>
2024-05-17 15:58:39 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 "use client"
 import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
   const router = useRouter()
-  const id = sessionStorage?.getItem('sessionId');
+  
+  const [id, setId] = useState("")
+  useEffect(() => {
+    const id = sessionStorage?.getItem('sessionId');
+    setId(id)
+  }, [])
   if(id){
     router.push('/home')
   }else{
