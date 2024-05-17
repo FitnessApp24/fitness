bd3ecb1898159dee0813a8231f65f6cfdbe688e8
Sushanth <sp480s@att.com>
2024-05-17 13:13:34 +0530

    update config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        OPENAI_API_KEY:process.env.OPENAI_API_KEY,
+        FIREBASE_API_KEY:process.env.FIREBASE_API_KEY,
+        AUTH_DOMAIN:process.env.AUTH_DOMAIN,
+        PROJECT_ID:process.env.PROJECT_ID,
+        STORAGE_BUCKET:process.env.STORAGE_BUCKET,
+        MSG_SENDER_ID:process.env.MSG_SENDER_ID,
+        APP_ID:process.env.APP_ID,
+        MEASURE_ID:process.env.MEASURE_ID
+    }
+}
 
 module.exports = nextConfig
